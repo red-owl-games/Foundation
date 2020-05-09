094a4f12161acdb7ed7b48384a4db0f8ea6ea2a0
Kyle Rockman <kyle.rockman@mac.com>
2020-05-09 12:28:40 -0500

    More utils added
    - Editor Progress bar display functions
    - array groupbycount extension

--- Editor/RedOwlEditor.cs
+++ Editor/RedOwlEditor.cs
@@ -1,7 +1,52 @@
+using System;
+using UnityEditor;
+
 namespace RedOwl.Core.Editor
 {
     public static partial class RedOwlEditor
     {
+        public static void Progress(string title, string body, params Action[] callbacks)
+        {
+            float steps = callbacks.Length;
+            EditorUtility.DisplayProgressBar(title, body, 0);
+            try
+            {
+                for (int i = 0; i < callbacks.Length; i++)
+                {
+                    callbacks[i]();
+                    EditorUtility.DisplayProgressBar(title, body, i/steps);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+        }
         
+        public static void Progress<TItem>(string title, string body, TItem[] items, Action<TItem> callback)
+        {
+            float steps = items.Length;
+            EditorUtility.DisplayProgressBar(title, body, 0);
+            try
+            {
+                for (int i = 0; i < steps; i++)
+                {
+                    callback(items[i]);
+                    EditorUtility.DisplayProgressBar(title, body, i/steps);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+        }
     }
 }
--- Engine/Extensions/ArrayExtensions.cs
+++ Engine/Extensions/ArrayExtensions.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
 using Unity.Mathematics;
 
 namespace RedOwl.Core
 {
     public static class ArrayExtensions
     {
+        /// <summary>
+        /// Performs a fisher-yates shuffle on the array.
+        /// </summary>
+        /// <typeparam name="T">The type of the array.</typeparam>
+        /// <param name="self">The array to shuffle.</param>
+        /// <param name="rng">The random number generator to use.</param>
+        /// <returns>A fisher-yates shuffled array.</returns>
         public static T[] Shuffle<T>(this T[] self, Random rng)
         {
             //Fisher Yates Shuffle
@@ -18,6 +27,21 @@ namespace RedOwl.Core
             }
 
             return self;
+        }
+        
+        /// <summary>
+        /// Splits an array into several smaller arrays.
+        /// </summary>
+        /// <typeparam name="T">The type of the array.</typeparam>
+        /// <param name="self">The array to split.</param>
+        /// <param name="size">The size of the smaller arrays.</param>
+        /// <returns>An array containing smaller arrays.</returns>
+        public static IEnumerable<IEnumerable<T>> GroupByCount<T>(this T[] self, int size)
+        {
+            for (int i = 0; i < (float)self.Length / size; i++)
+            {
+                yield return self.Skip(i * size).Take(size);
+            }
         }
     }
 }
