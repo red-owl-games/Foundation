928fea9b04d8d747ddb524a831e389ca83551e84
Kyle Rockman <kyle.rockman@mac.com>
2020-07-01 06:41:00 -0500

    Add Red Owl Splash Screen and a cool validaiton extension

---
+++ Engine/Extensions/ValidationExtensions.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace RedOwl.Core
+{
+    public static class ValidationExtensions
+    {
+        [Conditional("UNITY_EDITOR")]
+        public static void Requires<TSource, TComponent>(this TSource obj, TComponent component) where TSource : Object where TComponent : Object
+        {
+            if (component != null) return;
+            
+            Debug.LogFormat(LogType.Error, LogOption.NoStacktrace, obj,
+                $"<color=yellow>MISSING</color> - <color=blue>{typeof(TComponent)}</color> - on {obj}");
+
+            var mb = obj as MonoBehaviour;
+            if (mb != null) mb.enabled = false;
+        }
+    }
+}
