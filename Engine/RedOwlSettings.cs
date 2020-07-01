4ce201eec2a91a6b901b1b143f348fbd3e3f8d87
Kyle Rockman <kyle.rockman@mac.com>
2020-07-01 06:52:08 -0500

    Fix red owl settings initialization

--- Engine/RedOwlSettings.cs
+++ Engine/RedOwlSettings.cs
@@ -1,11 +1,24 @@
-using System;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using Sirenix.Utilities;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace RedOwl.Core
 {
+    internal static class RedOwlInitialization
+    {
+        // This needs to not be on the scriptable object so that it gets called
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            foreach (var singleton in RedOwlSettings.I.singletons)
+            {
+                Object.DontDestroyOnLoad(Object.Instantiate(singleton));
+            }
+        }
+    }
+    
     [HideMonoScript]
     [GlobalConfig("Assets/Resources/RedOwl/", UseAsset = true)]
     public partial class RedOwlSettings : Database<RedOwlSettings>
@@ -20,14 +33,5 @@ namespace RedOwl.Core
 
         [AssetsOnly]
         public List<GameObject> singletons;
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        private static void Initialize()
-        {
-            foreach (var singleton in I.singletons)
-            {
-                DontDestroyOnLoad(Instantiate(singleton));
-            }
-        }
     }
 }
